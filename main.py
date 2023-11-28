8127ff7da407634180cfe4731207e2979c2b52b3
LeoGoSummer <152259643+LeoGoSummer@users.noreply.github.com>
2023-11-28 09:16:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import chess
+board = chess.Board("r1bqkb1r/pppp1Qpp/2n2n2/4p3/2B1P3/8/PPPP1PPP/RNB1K1NR b KQkq - 0 4")
+print(board)
+r . b q k b . r
+p p p p . Q p p
+. . n . . n . .
+. . . . p . . .
+. . B . P . . .
+. . . . . . . .
+P P P P . P P P
+R N B . K . N R
